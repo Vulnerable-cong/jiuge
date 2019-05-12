46bb2228d7260d32812eda800a02c17b70a3909d
cong <591323205@qq.com>
2019-05-12 21:25:37 +0800

    纠错

--- src/main/java/com/jiuge/songs/service/Impl/UserServiceImpl.java
+++ src/main/java/com/jiuge/songs/service/Impl/UserServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -56,7 +55,6 @@ public class UserServiceImpl implements UserService {
 
         //注册时把当前时间放入用户表中，记为创建时间，只记到年月日
         Date date = new Date();
-        SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd");
 
         //默认前端用ajax校验过用户名是否重复，密码是否符合规则，所以在此直接进行插入
         User u = new User(account,password,date);
